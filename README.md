fafbda56687ebe82c7f7a88b7671c6b1b6cd98c8
Joaquin Samson <samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.com>
2024-12-29 21:50:11 +0800

    1 - Project Setup, backend, frontend initialization, routes, controllers, authentication, and models

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # mern-twitter-clone
-Clone Twitter (X) using the MERN (MongoDB, Express.js, React.js, Node.js) stack
+Clone Twitter using the MERN (MongoDB, Express.js, React.js, Node.js) stack
---
+++ backend/server.js
@@ -0,0 +1,37 @@
+// Packages
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
+
+// Routes
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+// DB
+import connectMongoDB from "./db/connectMongodb.js";
+
+dotenv.config();
+
+// Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+const PORT  = process.env.PORT || 7701;
+
+// middlewares
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+});
