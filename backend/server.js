f1b7414d3a70ade76b5d3437a5a0d23f15f2e82f
Joaquin Samson <samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.com>
2024-12-30 22:17:27 +0800

    1 - post model, user model, post routes, and post controllers added

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import {v2 as cloudinary} from "cloudinary";
 // Routes
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 // DB
 import connectMongoDB from "./db/connectMongodb.js";
@@ -27,9 +28,11 @@ const PORT  = process.env.PORT || 7701;
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cookieParser());
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
