4e58bd5accbf280843405607d6e03dca764e42f9
Joaquin Samson <samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.com>
2025-01-15 20:11:38 +0800

    2 - completed UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes, useLocation } from "react-router-dom";
+
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const location = useLocation();
+  const pathName = location.pathname;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='flex max-w-6xl mx-auto'>
+      {/* Sidebar */}
+      {pathName !== "/login" && pathName !== "/signup" && <Sidebar />}
 
-export default App
+      {/* Main Content Area */}
+      <div className='flex-1 flex'> {/* Make main content area flexible */}
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/notifications' element={<NotificationPage />} />
+          <Route path='/profile/:username' element={<ProfilePage />} />
+        </Routes>
+      </div>
+
+      {/* Right Panel */}
+      {pathName !== "/login" && pathName !== "/signup" && <RightPanel />}
+    </div>
+  );
+};
+
+export default App;
