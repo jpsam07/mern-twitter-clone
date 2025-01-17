535e902a42b8ead7b20472eacec2a7c511591aa7
Joaquin Samson <samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.com>
2025-01-17 23:12:20 +0800

    3 - added feature to fetch profile page posts and user info profiles

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,7 +3,7 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
+const Posts = ({ feedType, username, userId }) => {
 
 	const getPostEndpoint = () => {
 		switch(feedType) {
@@ -11,10 +11,14 @@ const Posts = ({ feedType }) => {
 				return "/api/posts/all";
 			case "following":
 				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
 			default:
 				return "/api/posts/all";
 		}
-	}
+	};
 
 	const POST_ENDPOINT = getPostEndpoint();
 
@@ -34,12 +38,12 @@ const Posts = ({ feedType }) => {
 			} catch (error) {
 				throw new Error(error);
 			}
-		}
+		} 
 	});
 
 	useEffect(() => {
 		refetch();
-	}, [feedType, refetch]);
+	}, [feedType, refetch, username]);
 
 	return (
 		<>
