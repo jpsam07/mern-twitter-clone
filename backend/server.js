fc043f8042cb8d9ebf2c29d6004aec230fc48046
Joaquin Samson <samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.comgit config --global user.email samson.joaquinpaulo@gmail.com>
2025-01-18 04:00:15 +0800

    4 - prepareed the app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // Packages
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -24,6 +25,7 @@ cloudinary.config({
 
 const app = express();
 const PORT  = process.env.PORT || 7701;
+const __dirname = path.resolve();
 
 // middlewares
 // limit should not be too high to prevent DOS attacks
@@ -36,6 +38,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
